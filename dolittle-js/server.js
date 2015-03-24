bb28f825605c3c51a50f850ef0ce55a20af2ebf4
mclarkk <meghan.leah.clark@gmail.com>
2015-03-23 23:10:46 -0400

    blowing away early attempt to implement dolittle in nodejs. Full Python now.

--- dolittle-js/server.js
+++
@@ -1,7 +0,0 @@
-var pushmi_pullyu = require('./lib/base/pushmi_pullyu.js');
-
-var object = new pushmi_pullyu('localhost', 1883, 'Alfred', 'in1', 'out1');
-//object.process_loop();
-
-   //start doctor
-   //start watchdog for doctor
