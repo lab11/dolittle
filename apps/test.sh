11ef0248e0850b373768063b3a7732d72522e34b
mclarkk <meghan.leah.clark@gmail.com>
2015-03-26 23:10:48 -0400

    testing rules for topic subscription wildcards

--- apps/test.sh
+++ apps/test.sh
@@ -1,10 +1,9 @@
 cd ../dolittle
 
 # SOURCES
-python -m src.lib.sources.test_message_source -name "Test Message" -out test -params '{"message": {"type": "turn off"}, "delay_secs": 1}'&
-python -m src.lib.sources.test_message_source -name "Test Message" -out test -params '{"message": {"type": "turn on"}, "delay_secs": 2}'&
+python -m src.lib.sources.test_message_source -name "Test Message" -out test/1 -params '{"message": {"type": "turn off"}, "delay_secs": 1}'&
+python -m src.lib.sources.test_message_source -name "Test Message" -out test/2 -params '{"message": {"type": "turn on"}, "delay_secs": 2}'&
 
 
 # SINKS
-# put wemo sinks here
-python -m src.lib.sinks.wemo_insight_sink -name 'Wemo Minion' -in test -params '{"ip_addr": "10.0.0.17"}' &
+#python -m src.lib.sinks.wemo_insight_sink -name 'Wemo Minion' -in test -params '{"ip_addr": "10.0.0.17"}' &
