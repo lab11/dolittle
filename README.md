00686731fc466d83eac5d61be9a0c146a1a42e5c
mclarkk <meghan.leah.clark@gmail.com>
2015-03-29 16:33:52 -0400

    restructured code, final cleanup

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 Code for Dolittle platform and Dolittle apps.
 
 More text to come later.
+
+dolittle-runtime
+
+dolittle-apps
+-- apps
+---- py-apps
+-- blocks
+---- py-blocks
+---- rb-blocks
+---- js-blocks
+---- cpp-blocks
+-- tests
--- dolittle-apps/apps/test.sh
+++
@@ -1,9 +0,0 @@
-cd ../dolittle
-
-# SOURCES
-python -m src.lib.sources.test_message_source -name "Test Message" -out test/1 -params '{"message": {"type": "turn off"}, "delay_secs": 1}'&
-python -m src.lib.sources.test_message_source -name "Test Message" -out test/2 -params '{"message": {"type": "turn on"}, "delay_secs": 2}'&
-
-
-# SINKS
-#python -m src.lib.sinks.wemo_insight_sink -name 'Wemo Minion' -in test -params '{"ip_addr": "10.0.0.17"}' &
