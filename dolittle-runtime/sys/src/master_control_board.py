e2bb160bc28e5969bc3c1a15dd7aaf1402a8a157
Meghan <meghan.leah.clark@gmail.com>
2015-07-15 01:46:55 -0400

    making the MCB do zeroconf

--- dolittle-runtime/sys/src/master_control_board.py
+++ dolittle-runtime/sys/src/master_control_board.py
@@ -4,13 +4,28 @@ class MasterControlBoard(Block):
 	def __init__(self):
         super(Processor, self).__init__()
 
+        # code directory
+        code_directory = {}
+
+        # currently running apps
+
+        # active streams
+
+        # stream schema
+
+        # connect to LAN Dolittle master service or become it.
+
+
 	def process(self, obj):
 		new_obj = str(obj)
 		# if msg_type == app:
 			# for block in blocks, instantiate.
 			# send status
-		# if msg_type == status_request:
+		# if msg_type == app_info_request:
 			# send status
+		# if msg_type == stream_info_request:
+			# send status
+		# if msg_type == 
 
 if __name__ == "__main__":
 	mcb = MasterControlBoard()
