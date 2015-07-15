aca3c13b41c608ed21ebf7b576e79308f891db90
mclarkk <meghan.leah.clark@gmail.com>
2015-07-14 23:24:03 -0400

    more socket bullshit

--- dolittle-apps/blocks/sinks/socket_sink.py
+++ dolittle-apps/blocks/sinks/socket_sink.py
@@ -11,10 +11,12 @@ class SocketSink(Sink):
         self.port = self.params['port']
 
     def process(self, msg_json):
+        print("sending...")
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.dest, self.port))
         self.s.send(json.dumps(msg_json))
         self.s.close()
+        print("done...")
 
 if __name__ == "__main__":
     block = SocketSink()
--- dolittle-apps/blocks/sources/socket_source.py
+++ dolittle-apps/blocks/sources/socket_source.py
@@ -10,14 +10,17 @@ class SocketSource(PollingSource):
         self.TCP_IP = self.params['host']
         self.TCP_PORT = self.params['port']
         
-        self.start_polling()
-
-    def poll(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((self.TCP_IP, self.TCP_PORT))
         self.s.listen(1)
+
+        self.start_polling()
+
+    def poll(self):
+        print("listening...")
         self.conn, addr = self.s.accept()
+        print("accepted...")
         while 1:
             data = self.conn.recv(BUFFER_SIZE)
             if not data: break
@@ -26,7 +29,7 @@ class SocketSource(PollingSource):
             except:
                 print("Received non-JSON data from {}: {}".format(addr, data))
         self.conn.close()
-        self.s.close()
+        #self.s.close()
         return 0
 
 
